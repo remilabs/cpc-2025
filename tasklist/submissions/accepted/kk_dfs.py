ec6c4645168673e84dd02d5ed7f9a7abc7eac4cd
kmkinney <kevinmk712@gmail.com>
2025-02-18 11:02:36 -0700

    First solution attempt

--- tasklist/submissions/accepted/kk_dfs.py
+++ tasklist/submissions/accepted/kk_dfs.py
@@ -1,1 +1,28 @@
-print("todo")
+n, m = map(int, input().split())
+
+t = {i: int(input()) for i in range(n)}
+g = {i: [] for i in range(n)}
+is_sink = {i: True for i in range(n)}
+
+for _ in range(m):
+    ai, bi = map(int, input().split())
+    g[bi].append(ai)
+    is_sink[ai] = False
+
+
+def dfs(v, total):
+    if len(g[v]) == 0:
+        return t[v]
+    path_totals = []
+    for c in g[v]:
+        pt = dfs(c, total) + t[v]
+        path_totals.append(pt)
+    return max(path_totals)
+
+
+totals = []
+for i in range(n):
+    if is_sink[i]:
+        totals.append(dfs(i, 0))
+
+print(max(totals))
