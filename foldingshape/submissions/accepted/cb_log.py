2440508a876a884769b5e7f36e55e52a755ceeab
kmkinney <kevinmk712@gmail.com>
2025-02-20 10:05:03 -0700

    This worked against all test cases and a bunch I randomly generated

---
+++ foldingshape/submissions/accepted/cb_log.py
@@ -0,0 +1,17 @@
+import math
+
+
+def solve():
+    h, w = map(int, input().split())
+    hh, ww = map(int, input().split())
+    h, w = max(h, w), min(h, w)
+    hh, ww = max(hh, ww), min(hh, ww)
+    count = 0
+    if h > hh:
+        count += math.ceil(math.log2(h) - math.log2(hh))
+    if w > ww:
+        count += math.ceil(math.log2(w) - math.log2(ww))
+    return count
+
+
+print(solve())
