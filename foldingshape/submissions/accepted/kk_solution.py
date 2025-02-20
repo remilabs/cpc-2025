5f0b74276956f5eb90b612088de96e2658996fd6
kmkinney <kevinmk712@gmail.com>
2025-02-19 17:35:32 -0700

    Update my solution to account for rotating

--- foldingshape/submissions/accepted/kk_solution.py
+++ foldingshape/submissions/accepted/kk_solution.py
@@ -4,13 +4,18 @@ import math
 def solve():
     h, w = map(int, input().split())
     hh, ww = map(int, input().split())
+    h, w = max(h, w), min(h, w)
+    hh, ww = max(hh, ww), min(hh, ww)
     count = 0
-    while h > hh:
-        h = math.ceil(h / 2)
-        count += 1
-    while w > ww:
-        w = math.ceil(w / 2)
-        count += 1
+    while h > hh or w > ww:
+        if h > hh:
+            h = math.ceil(h / 2)
+            count += 1
+        if w > ww:
+            w = math.ceil(w / 2)
+            count += 1
+        h = max(h, w)
+        w = min(h, w)
     return count
 
 
