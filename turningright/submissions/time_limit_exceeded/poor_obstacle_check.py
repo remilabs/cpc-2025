2f618ce6f91ec68d72a1b1b2bbf7371ae2552437
isaacerdman <isaac.erdman6@gmail.com>
2025-02-24 22:47:57 -0700

    turningright submission, tle submission, additional test case

---
+++ turningright/submissions/time_limit_exceeded/poor_obstacle_check.py
@@ -0,0 +1,31 @@
+from functools import lru_cache
+import time
+
+def rotateccw(p,t):
+    if not t:
+        return p
+    return rotateccw((-p[1],p[0]),t-1)
+
+@lru_cache(None)
+def solve(x,y):
+    if y==0 or x==0: return 1
+    q=0
+    if x>0:q=1 if y>0 else 2
+    elif y<0:q=3
+    x,y=rotateccw((x,y),q)
+    c=0
+    for i in range(0,-x):
+        for j in range(x,i):
+            if (j,y) in obs[q]:break
+        else: c += solve(*rotateccw((i,y),4-q))
+    return c % MOD
+
+MOD=10**9+7
+x,y,n=map(int,input().split())
+ob=set(tuple(map(int,input().split())) for _ in range(n))
+obs={}
+obs={i:set() for i in range(4)}
+for i in range(4):
+    for o in ob:
+        obs[i].add(rotateccw(o,i))
+print(solve(x,y))
