dd577f1f51a35adb2bf178b90c8d28bca0573235
Josh Taylor <joshbtay@gmail.com>
2025-02-26 15:30:09 -0700

    reduce bounds, overhaul test cases for turningright

--- turningright/data/secret/gen.py
+++ turningright/data/secret/gen.py
@@ -1,6 +1,6 @@
 from random import randint
-n = 2000
-m = 200
+n = 1000
+m = 100
 print(m,m,n)
 b = set()
 r = lambda: randint(1,m-1) * [-1, 1][randint(0,1)]
--- turningright/data/secret/gen2.py
+++ turningright/data/secret/gen2.py
@@ -1,11 +1,11 @@
-x = y = 200
+x = y = 100
 n = 10000
 print(x, y, n)
 
 total = 0
 for i in range(-x, 0):
   if total == n: break
-  for j in range(y, 0, -1):
+  for j in range(y, -y, -1):
     print(i,j)
     total += 1
     if total == n: break
